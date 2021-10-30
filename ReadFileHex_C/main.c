f32264a509c62059af916e6ebcc025c08f687eee
LX <alexandermathieu@googlemail.com>
2021-10-30 07:17:21 +0200

    Initial Commit

---
+++ ReadFileHex_C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ReadFileHex_C
+//
+//  Created by Alexander Mathieu on 30.10.21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
